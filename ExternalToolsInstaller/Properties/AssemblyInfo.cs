200f0169970c32157a6379b1781beef4fdaf9dbe
Marco Russo <marco.russo@loader.it>
2020-08-03 18:58:31 +0200

    Updated about

--- ExternalToolsInstaller/Properties/AssemblyInfo.cs
+++ ExternalToolsInstaller/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExternalToolsInstaller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Configuration for External Tools in Power BI Desktop")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExternalToolsInstaller")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
